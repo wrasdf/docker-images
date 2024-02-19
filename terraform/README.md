340a037253b89e2a2a41faf49494834d04509ac4
wrasdf <452784+wrasdf@users.noreply.github.com>
2024-02-19 22:41:19 +1100

    update terraform image

---
+++ terraform/README.md
@@ -0,0 +1,4 @@
+### Details
+- gomplate: 3.11.7
+- yq: 4.21.1
+- terraform: 1.7.4
--- terraform/bin/release.sh
+++ terraform/bin/release.sh
@@ -4,14 +4,14 @@ set -euo pipefail
 
 if [ "$#" -lt 1 ]; then
   echo
-  echo "usage: release.sh <ImageTag>"
-  echo "  ie. release.sh v0.1.0"
+  echo "usage: ./bin/release.sh <ImageTag>"
+  echo "  ie. ./bin/release.sh v0.1.0"
   exit 255
 fi
 
-# docker-compose build tf-toolkit
-# docker-compose push tf-toolkit
+# docker-compose build tf
+# docker-compose push tf
 
 APP_TAG=${1}
-APP_TAG=${APP_TAG} docker-compose build tf-toolkit
-APP_TAG=${APP_TAG} docker-compose push tf-toolkit
+APP_TAG=${APP_TAG} docker-compose build tf
+APP_TAG=${APP_TAG} docker-compose push tf
